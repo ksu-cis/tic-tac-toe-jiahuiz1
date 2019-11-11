1411d3628be83434b449b4eaf0346629b14a2431
Jiahui Zhao <jiahuiz@users.campus>
2019-11-11 17:25:43 -0600

    Tic-Tac-Toe

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,56 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var currentTurn = "X";
+
+document.getElementById("turn").innerText = "It is player " + currentTurn + "'s turn";//or we can use string template `It is player ${currentText} 's turn`
+
+var squares = document.getElementsByClassName("square");
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', onClick);
+}
+
+function onClick(event) {
+    event.preventDefault();
+    if (!event.target.innerText) {
+        event.target.innerText = currentTurn;
+        if (!checkForWin()) {
+            nextTurn();
+        }     
+    }
+}
+
+function nextTurn() {
+
+    currentTurn = (currentTurn === "X") ? "O" : "X";
+    document.getElementById("turn").innerText = "It is player " + currentTurn + "'s turn";
+}
+
+function declareWinner() {
+    document.getElementById("turn").innerText = "Player " + currentTurn + " wins!";
+    currentTurn = "";
+}
+
+function checkForWin() {
+    //check for rows
+    for (var i = 0; i < 9; i += 3) {
+        if (squares[i].innerText && squares[i].innerText === squares[i+1].innerText && squares[i+1].innerText === squares[i+2].innerText) {
+            declareWinner();
+            return true;
+        }
+    }
+
+    //check for columns
+    for (var j = 0; j < 3; j += 1) {
+        if (squares[j].innerText && squares[j].innerText === squares[j + 3].innerText && squares[j + 3].innerText === squares[j + 2].innerText) {
+            declareWinner();
+            return true;
+        }
+    }
+
+    //check for diagonals
+
+    return false;
+
+}
