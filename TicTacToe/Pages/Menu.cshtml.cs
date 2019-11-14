6e0277eec76b18f5aa0d7ce63faf751979dc1547
Jiahui Zhao <jiahuiz@users.campus>
2019-11-14 12:23:06 -0600

    List the items need to do

--- TicTacToe/Pages/Menu.cshtml.cs
+++ TicTacToe/Pages/Menu.cshtml.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DinoDiner.Menu;
 
 namespace TicTacToe.Pages
 {
     public class MenuModel : PageModel
     {
+        public Menu menu { get; }
+
+        public double Price
+        {
+            get
+            {
+                return menu.Price
+            }
+        }
+
         public void OnGet()
         {
 
